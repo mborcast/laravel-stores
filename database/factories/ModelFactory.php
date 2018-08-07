e36cc374643aba3b9a440217c412f77d38523f0a
Manuel Borges <mborges@gentera.com.mx>
2018-08-07 13:28:31 -0500

    Create resource factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -10,15 +10,25 @@
 | database. Just tell the factory how a default model should look.
 |
 */
-
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(LaravelStores\User::class, function (Faker\Generator $faker) {
-    static $password;
-
+$factory->define(
+    LaravelStores\Web\Resources\Customers\Customer::class, 
+    function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'name' => implode(' ', [$faker->firstName, $faker->lastName])
+    ];
+});
+$factory->define(
+    LaravelStores\Web\Resources\Stores\Store::class, 
+    function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->city
+    ];
+});
+$factory->define(
+    LaravelStores\Web\Resources\Products\Product::class, 
+    function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->word,
+        'price' => $faker->randomFloat(2, 50, 1000)
     ];
 });
