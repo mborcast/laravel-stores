19a091c5f6983c88d1e8df8f91ead7983d590d2e
Manuel Borges <mborges@gentera.com.mx>
2018-08-07 13:57:11 -0500

    Add sales foreign relationships

---
+++ database/migrations/2018_08_07_184223_create_sales_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('store_id')->unsigned();
+            $table->integer('customer_id')->unsigned();
+            $table->dateTime('date');	
+            $table->timestamps();
+        });
+        Schema::table('sales', function($table) {
+            $table->foreign('store_id')->references('id')->on('stores')->onDelete('cascade');
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
