e62cd5fe3d3bbda17e989030f691c257bd36b5bd
Manuel Borges <mborges@gentera.com.mx>
2018-08-07 10:55:22 -0500

    Initialize product model

--- app/Web/Resources/Products/Product.php
+++ app/Web/Resources/Products/Product.php
@@ -5,4 +5,11 @@ namespace LaravelStores\Web\Resources\Products;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model {
+    protected $guarded = [
+        'id'
+    ];
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
 }
