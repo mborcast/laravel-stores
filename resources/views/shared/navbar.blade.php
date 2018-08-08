f960446855fa718c2a97310476456aa5d15a38bb
Manuel Borges ºCº <mborcast@outlook.com>
2018-08-07 23:48:20 -0500

    Add navbar

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -1,9 +1,22 @@
-<ul>
-    <li>
-        <a href="/customers">Customers</a>
-        <a href="/details">Details</a>
-        <a href="/products">Products</a>
-        <a href="/sales">Sales</a>
-        <a href="/stores">Stores</a>
-    </li>
-</ul>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#"><i class="fas fa-store"></i> Laravel Stores</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <!-- <li class="nav-item">
+                <a class="nav-link" href="/customers"><i class="fas fa-user-friends"></i> Customers</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/details"><i class="fas fa-user-friends"></i> Details</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/sales"><i class="fas fa-user-friends"></i> Sales</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/stores"><i class="fas fa-user-friends"></i>Stores</a>
+            </li> -->
+        </ul>
+    </div>
+</nav>
