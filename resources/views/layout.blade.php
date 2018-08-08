a3baec700210bb4a2693b1b7a792bda25498697a
Manuel Borges ºCº <mborcast@outlook.com>
2018-08-07 23:47:40 -0500

    Add fontawesome

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,14 +7,16 @@
 
 	<title>Laravel Stores – @yield('title')</title>
 
-	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+	
 	@yield('styles')
 </head>
 <body>
 	@include('shared.navbar')
 	@yield('content')
 	<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-    <script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
+	<script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
+	<script defer src="https://use.fontawesome.com/releases/v5.2.0/js/all.js" integrity="sha384-4oV5EgaV02iISL2ban6c/RmotsABqE4yZxZLcYMAdG7FAPsyHYAPpywE9PJo+Khy" crossorigin="anonymous"></script>
 	<script>
 		$.ajaxSetup
 		({
