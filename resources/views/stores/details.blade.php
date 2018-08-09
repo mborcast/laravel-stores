ecc030cdeca4583fc044005b44326f1210aecf01
Manuel Borges ºCº <mborcast@outlook.com>
2018-08-08 23:57:48 -0500

    Create stores detail view

--- resources/views/stores/details.blade.php
+++ resources/views/stores/details.blade.php
@@ -1,27 +1,65 @@
-@extends('layout')
-@section('title', '')
+@extends('layout', ['title' => $store->name])
 @section('content')
 
-<section class="container">
-    <header class="row">
-        <div class="col">
-            <h5>{{ $store->name }}</h5>
-        </div>
-    </header>
-    <div class="row">
-        <div class="col">
-            <ul>
-                @foreach ($store->customers as $customer)
-                <li>
-                    {{ $customer->id }} - 
-                    <a href="{{ route('customers-details', $customer->id) }}">
-                        {{ $customer->name }}
-                    </a>
-                </li>
-                @endforeach
-            </ul>
-        </div>
+<div class="details-grid">
+    <section class="top">
+      <div class="container">
+        <header class="store-name">
+          <p>Store</p>
+          <h2>{{ $store->name }}</h2>
+        </header>
+        <ul class="tabs">
+          <li class="tab-item" data-outlet="customers">
+            <i class="fas fa-users"></i>
+            <span>Customers</span>
+          </li>
+          <li class="tab-item" data-outlet="sales">
+            <i class="fas fa-box-open"></i>
+            <span>Sales</span>
+          </li>
+        </ul>
+      </div>
+    </section>
+    <div class="container">
+      <div class="outlet">
+        <table class="customers">
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+          </tr>
+          @foreach ($store->customers as $customer)
+          <tr>
+            <td>{{ $customer->id }}</td>
+            <td>
+              <a href="{{ route('customers-details', $customer->id) }}">
+                {{ $customer->name }}
+              </a>
+            </td>
+          </tr>
+          @endforeach
+        </table>
+        <table class="sales">
+          <tr>
+            <th>ID</th>
+            <th>Date</th>
+          </tr>
+          @foreach ($store->sales as $sale)
+          <tr>
+            <td>{{ $sale->id }}</td>
+            <td>
+              <a href="{{ route('customers-details', $sale->id) }}">
+                {{ $sale->date }}
+              </a>
+            </td>
+          </tr>
+          @endforeach
+        </table>
+      </div>
     </div>
-</section>
+</div>
 
+@endsection
+
+@section('scripts')
+<script src="{{ asset('js/details.js') }}"></script>
 @endsection
