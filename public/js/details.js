bb940b0a2f531d375bcd9242b31c9ad3dc339cfe
Manuel Borges ºCº <mborcast@outlook.com>
2018-08-08 23:57:04 -0500

    Create tab item toggler script

---
+++ public/js/details.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    handleTabItemClick($('.tab-item:first'));
+    $('.tab-item').click(function() {
+        handleTabItemClick($(this));
+    });
+});
+function handleTabItemClick(element) {
+    selectTabItem($(element));
+    showOutlet($(element));
+}
+function selectTabItem(element) {
+    $('.tab-item-selected').removeClass('tab-item-selected');
+    $(element).addClass('tab-item-selected');
+}
+function showOutlet(element) {
+    $('.outlet table').hide();
+    $('.' + $(element).attr('data-outlet')).show();
+}
