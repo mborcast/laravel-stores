16adec9eb2ec2a84e041d84ffd6851f134ab780a
Manuel Borges ºCº <mborcast@outlook.com>
2018-08-09 00:32:23 -0500

    Create details view for customers resource

--- resources/views/customers/details.blade.php
+++ resources/views/customers/details.blade.php
@@ -1,17 +1,46 @@
-@extends('layout')
-@section('title', '')
+@extends('layout', ['title' => $customer->name])
 @section('content')
 
-<section class="container">
-    <header class="row">
-        <div class="col">
-            <h5>{{ $customer->name }}</h5>
-        </div>
-    </header>
-    <div class="row">
-        <div class="col">
-        </div>
+<div class="details-grid">
+    <section class="top">
+      <div class="container">
+        <header class="details-name">
+          <p>Customer</p>
+          <h2>{{ $customer->name }}</h2>
+        </header>
+        <ul class="tabs">
+          <li class="tab-item" data-outlet="sales">
+            <i class="fas fa-box-open"></i>
+            <span>Sales</span>
+          </li>
+        </ul>
+      </div>
+    </section>
+    <div class="container">
+      <div class="outlet">
+        <table class="sales">
+          <tr>
+            <th>ID</th>
+            <th>Store</th>
+            <th>Date</th>
+          </tr>
+          @foreach ($customer->sales as $sale)
+          <tr>
+            <td>{{ $sale->id }}</td>
+            <td>
+              <a href="{{ route('stores-details', $sale->store->id) }}">
+                {{ $sale->store->name }}
+              </a>
+            <td>{{ $sale->date }}</td>
+          </tr>
+          @endforeach
+        </table>
+      </div>
     </div>
-</section>
+</div>
 
+@endsection
+
+@section('scripts')
+<script src="{{ asset('js/details.js') }}"></script>
 @endsection
