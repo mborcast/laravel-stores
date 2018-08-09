d5bafa72dbf0ee69a9b1b1c6c54a5764aba27977
Manuel Borges ºCº <mborcast@outlook.com>
2018-08-08 23:57:39 -0500

    Create custom navbar

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -1,22 +1,5 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="/stores"><i class="fas fa-store"></i> Laravel Stores</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="/customers"><i class="fas fa-user-friends"></i> Customers</a>
-            </li>
-            <!-- <li class="nav-item">
-                <a class="nav-link" href="/details"><i class="fas fa-user-friends"></i> Details</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/sales"><i class="fas fa-user-friends"></i> Sales</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/stores"><i class="fas fa-user-friends"></i>Stores</a>
-            </li> -->
-        </ul>
+<nav class="navbar">
+    <div class="container">
+        <a href="/stores"><i class="fas fa-store"></i> Laravel Stores</a>
     </div>
 </nav>
