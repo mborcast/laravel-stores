d3624c9f8c28254e44c9dd97017a4c40bc43ba7f
Manuel Borges ºCº <mborcast@outlook.com>
2018-08-10 21:52:25 -0500

    Add styled markup for sales index page

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -10,17 +10,20 @@
       </div>
     </section>
     <div class="container">
-      <div class="index-grid">
+      <div class="sales-grid">
         @if(empty($sales))
           <p>No sales available.</p>
         @else    
           @foreach ($sales as $sale)
-          <a class="index-item" href="{{ route('sales-details', $sale->id) }}">
-            <p>{{ $sale->date->format('d-M-Y') }}</p>
-            <p>{{ $sale->date->format('H:m:s') }}</p>
-            <p>{{ $sale->products[0]->name }}</p>
-            <p>${{ $sale->products[0]->price }}</p>
-            <p>{{ $sale->products[0]->pivot->units }}</p>
+          <a class="sales-item" href="{{ route('sales-details', $sale->id) }}">
+            <header>
+              <p class="units">{{ $sale->products[0]->pivot->units }} units</p>
+              <p class="product">{{ $sale->products[0]->name }}</p>
+            </header>
+            <div>
+              <p class="date">{{ $sale->date->format('d-M-Y') }}</p>
+              <p class="price">{{ $sale->products[0]->price }}</p>
+            </div>
           </a>
           @endforeach
         @endif
