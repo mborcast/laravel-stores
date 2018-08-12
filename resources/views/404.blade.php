f4464625a419b5a9868db2c75c89e4e609ec0085
Manuel Borges ºCº <mborcast@outlook.com>
2018-08-12 13:24:01 -0500

    Remove scrips section

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,14 +1,9 @@
 @extends('layout', ['title' => '404'])
 @section('content')
-
 <section class="not-found">
   <header>
     <p class="code">404</p>
     <p>Resource not found</p>
   </header>
 </section>
-
-@endsection
-
-@section('scripts')
 @endsection
