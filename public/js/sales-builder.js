9d63b9318a8649f3a0b2bbd3418ffb7e5354bf04
Manuel Borges ºCº <mborcast@outlook.com>
2018-08-12 22:09:49 -0500

    Set jimmy fixes

--- public/js/sales-builder.js
+++ public/js/sales-builder.js
@@ -1,19 +1,15 @@
 function build(sale) {
   return '<div class="sales-item">'+
           '<aside>'+
-            '<p class="date">'+(new Date(sale.date)).toLocaleDateString("en-GB", { year: 'numeric', month: 'short', day: 'numeric' })+'</p>'+
             '<p class="units">'+
               '<i class="fas fa-box-open"></i>'+
-              '<span class="times">'+
-                '<i class="fas fa-times"></i>'+
-              '</span>'+
+              '<span class="times"><i class="fas fa-times"></i></span>'+
               sale.products[0].pivot.units+
             '</p>'+
+            '<p class="date">'+(new Date(sale.date)).toLocaleDateString("en-GB", { year: 'numeric', month: 'short', day: 'numeric' })+'</p>'+
             '<p class="product">'+sale.products[0].name+'</p>'+
-            '<p class="store">'+sale.store.name+'</p>'+
-            '<p class="customer">'+sale.customer.name+'</p>'+
           '</aside>'+
-          '<a href="sales/'+sale.id+'">'+
+          '<a href="/sales/'+sale.id+'">'+
             '<button type="button" class="mini button primary"><i class="fas fa-eye"></i></button>'+
           '</a>'+
           '<label class="control">'+
